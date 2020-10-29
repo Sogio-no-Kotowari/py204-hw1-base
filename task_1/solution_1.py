d482cf6aae99bacdd74845a0e0083ea874fe4d9b
Sogio-no-Kotowari <71990088+Sogio-no-Kotowari@users.noreply.github.com>
2020-10-29 21:00:18 +0300

    Update solution_1.py

--- task_1/solution_1.py
+++ task_1/solution_1.py
@@ -1,1 +1,8 @@
-# your code here
+a=input("введите последовательность")
+max=0
+for element in a:
+    my_int_element= int(element)
+    if my_int_element>=max:
+        max=my_int_element
+
+print ("max=",max)

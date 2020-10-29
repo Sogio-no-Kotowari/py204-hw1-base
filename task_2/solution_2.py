0b0ca10d8e1abd4beabdb936aedee94df036cb29
Sogio-no-Kotowari <71990088+Sogio-no-Kotowari@users.noreply.github.com>
2020-10-30 01:14:21 +0300

    Update solution_2.py

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -1,4 +1,14 @@
-# your code here
-user_input = input()
-# your code here
-print(most_frequent_character)
+a=input()
+max=0
+for element in a:
+  
+  k=0  
+  for symbol in a:
+    if element==symbol:
+          k=k+1
+        
+    if k>=max:
+        max=k
+        g=element  
+print ("самый частый элемент - ", g)
+ 
